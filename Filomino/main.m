331f4b313c1a77b83dfe279cf2792500cd497b4a
Jeffrey Monte <jmonte_x2@Jeffreys-MacBook-Pro.local>
2012-06-17 01:18:04 +0800

    Initial Commit

---
+++ Filomino/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Filomino
+//
+//  Created by Jeffrey Monte on 17/6/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
